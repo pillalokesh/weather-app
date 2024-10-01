b4847dd8b05f452efd80f6d4d9cfe9fa7e9c9ccc
pillalokesh <163496249+pillalokesh@users.noreply.github.com>
2024-10-01 15:05:02 +0530

    Add files via upload

---
+++ gaye.js
@@ -0,0 +1,83 @@
+const apiKey = "72f4a9b91b06f288f4ba9928f9c85cea";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+async function checkWeather(city){
+   const response = await  fetch(apiUrl + city +`&appid=${apiKey}`);
+
+if(response.status == 404){
+   document.querySelector(".error").style.display = "block";
+   document.querySelector(".weather").style.display = "none";
+}else{
+   var data = await response.json();
+
+
+
+   document.querySelector(".city").innerHTML = data.name;
+   document.querySelector(".temp").innerHTML =  Math.round(data.main.temp) + "°C";
+   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h" ;
+
+
+   if(data.weather[0].main =="Clouds"){
+       weatherIcon.src ="clouds.jpeg";
+   }
+   else if(data.weather[0].main == "Clear"){
+       weatherIcon.src ="clear.png";
+   }
+   else if(data.weather[0].main =="Rain"){
+       weatherIcon.src ="Rain.jpeg";
+   }
+   else if(data.weather[0].main =="Drizzle"){
+       weatherIcon.src ="drizzle.png"; 
+}
+else if(data.weather[0].main =="mist"){
+   weatherIcon.src ="mist.jpeg";
+}
+document.querySelector(".weather").style.display ="block";
+document.querySelector(".error").style.display = "none";
+
+}
+
+   var data = await response.json();
+
+
+
+   document.querySelector(".city").innerHTML = data.name;
+   document.querySelector(".temp").innerHTML =  Math.round(data.main.temp) + "°C";
+   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+   document.querySelector(".wind").innerHTML = data.wind.speed + "km/h" ;
+
+
+   if(data.weather[0].main =="Clouds"){
+       weatherIcon.src ="clouds.jpeg";
+   }
+   else if(data.weather[0].main == "Clear"){
+       weatherIcon.src ="clear.png";
+   }
+   else if(data.weather[0].main =="Rain"){
+       weatherIcon.src ="Rain.jpeg";
+   }
+   else if(data.weather[0].main =="Drizzle"){
+       weatherIcon.src ="drizzle.png"; 
+}
+else if(data.weather[0].main =="mist"){
+   weatherIcon.src ="mist.jpeg";
+}
+document.querySelector(".weather").style.display ="block";
+}
+
+
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+}
+)
+
+   
+
